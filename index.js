e37e784fa56a23e9ef7c9b5fe42a26c204e8c1e1
Harsh Haria <harshharia1234@gmail.com>
2024-09-16 03:30:08 +0530

    redis connection paramters changed to avoid errors while connecting

--- index.js
+++ index.js
@@ -31,8 +31,10 @@ else {
     const app = express();
 
     const redisClient = redis.createClient({
-        host: 'redis',
-        port: 6379
+        socket: {
+            host: 'redis',
+            port: 6379
+        }
     });
 
     redisClient.on('error', (err) => console.log('Redis Client Error', err));
